7fdb0f2db3f3e4930e7be4b06b4ebe29e1122769
gilbertf <gilbert@erlangen.ccc.de>
2013-05-31 10:10:38 +0200

    updated plugin name

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require 'redmine'
 Redmine::Plugin.register :redmine_sidebar_toc_pt do
-  name 'Redmine Sidebar Table of Content and Project Tree plugin'
+  name 'Sidebar Table of Content and Project Tree plugin'
   author 'Gilbert Johannes Martin Forkel'
   description 'navigation by the header of the anchor links in the wiki sidebar and show project tree'
   version '1.1.0'
