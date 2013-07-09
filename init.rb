2f188c30d1b896b0db66fda73691d455a23ba182
gilbert <gilbert@erlangen.ccc.de>
2013-07-09 20:54:10 +0200

    updated version info, to represent the last two fixes

--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ Redmine::Plugin.register :redmine_sidebar_toc_pt do
   name 'Sidebar Table of Content and Project Tree plugin'
   author 'Gilbert Johannes Martin Forkel'
   description 'navigation by the header of the anchor links in the wiki sidebar and show project tree'
-  version '1.2.0'
+  version '1.3.0'
   url 'https://github.com/gilbertf/redmine_sidebar_toc_pt'
 end
